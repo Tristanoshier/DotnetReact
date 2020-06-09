7be60fc65f527d12c41f73c1496b3cbf5120be8c
Tristanoshier <tsoshier@bsu.edu>
2020-06-09 15:29:20 -0400

    add functionality to buttons

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import './styles.css';
 import axios from 'axios';
-import { List, Container } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import { IActivity } from '../models/activity';
 import { NavBar } from '../../features/nav/NavBar';
 import { ActivityDashboard } from '../../features/activities/dashboard/ActivityDashboard';
@@ -9,6 +9,17 @@ import { ActivityDashboard } from '../../features/activities/dashboard/ActivityD
 const App = () => {
 
   const [activities, setActivities] = useState<IActivity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
+  const [editMode, setEditMode] = useState(false);
+
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.filter(a => a.id === id)[0])
+  }
+
+  const handleOpenCreateForm = () => {
+    setSelectedActivity(null);
+    setEditMode(true);
+  }
 
   useEffect(() => {
     axios.get<IActivity[]>('http://localhost:5000/api/activities')
@@ -19,9 +30,16 @@ const App = () => {
 
   return (
     <Fragment>
-      <NavBar />
+      <NavBar openCreateForm={handleOpenCreateForm} />
       <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard activities={activities} />
+        <ActivityDashboard 
+          activities={activities} 
+          selectActivity={handleSelectActivity} 
+          selectedActivity={selectedActivity}
+          setSelectedActivity={setSelectedActivity}
+          editMode={editMode}
+          setEditMode={setEditMode}
+        />
       </Container>
     </Fragment>
   );
