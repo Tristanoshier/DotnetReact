f5cdeb5ec53e5113a34d57e7853bc79c9b6956ea
Tristanoshier <tsoshier@bsu.edu>
2020-06-09 18:44:08 -0400

    add axios to apply full CRUD to our server

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import './styles.css';
-import axios from 'axios';
 import { Container } from 'semantic-ui-react';
 import { IActivity } from '../models/activity';
 import { NavBar } from '../../features/nav/NavBar';
 import { ActivityDashboard } from '../../features/activities/dashboard/ActivityDashboard';
+import agent from '../api/agent';
 
 const App = () => {
 
@@ -23,26 +23,32 @@ const App = () => {
   }
 
   const handleCreateActivity = (activity: IActivity) => {
-    setActivities([...activities, activity]);
-    setSelectedActivity(activity);
-    setEditMode(false);
+    agent.Activities.create(activity).then(() => {
+      setActivities([...activities, activity]);
+      setSelectedActivity(activity);
+      setEditMode(false);
+    });
   }
 
   const handleEditActivity = (activity: IActivity) => {
-    setActivities([...activities.filter(a => a.id !== activity.id), activity]);
-    setSelectedActivity(activity);
-    setEditMode(false);
+    agent.Activities.update(activity).then(() => {
+      setActivities([...activities.filter(a => a.id !== activity.id), activity]);
+      setSelectedActivity(activity);
+      setEditMode(false);
+    });
   }
 
   const handleDeleteActivity = (id: string) => {
-    setActivities([...activities.filter(a => a.id !== id)]);
+    agent.Activities.delete(id).then(() => {
+      setActivities([...activities.filter(a => a.id !== id)]);
+    });
   }
 
   useEffect(() => {
-    axios.get<IActivity[]>('http://localhost:5000/api/activities')
+    agent.Activities.list()
       .then((response) => {
-        let activities : IActivity[] = [];
-        response.data.forEach(activity => {
+        let activities: IActivity[] = [];
+        response.forEach(activity => {
           activity.date = activity.date.split('.')[0];
           activities.push(activity);
         })
@@ -54,9 +60,9 @@ const App = () => {
     <Fragment>
       <NavBar openCreateForm={handleOpenCreateForm} />
       <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard 
-          activities={activities} 
-          selectActivity={handleSelectActivity} 
+        <ActivityDashboard
+          activities={activities}
+          selectActivity={handleSelectActivity}
           selectedActivity={selectedActivity}
           setSelectedActivity={setSelectedActivity}
           editMode={editMode}
