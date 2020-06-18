1cc9a7c98764ecd801c87afdcf8dd00286fa2f7a
Tristanoshier <tsoshier@bsu.edu>
2020-06-18 17:03:17 -0400

    initialize form with data

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,6 +1,6 @@
 import React, { useState, FormEvent, useContext, useEffect } from 'react'
 import { Segment, Form, Button, Grid } from 'semantic-ui-react'
-import { IActivity, IActivityFormValues } from '../../../app/models/activity'
+import { IActivity, IActivityFormValues, ActivityFormValues } from '../../../app/models/activity'
 import { v4 as uuid } from 'uuid';
 import ActivityStore from '../../../app/stores/activityStore'
 import { observer } from 'mobx-react-lite';
@@ -21,25 +21,15 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({ match, hist
     const activityStore = useContext(ActivityStore);
     const { createActivity, editActivity, submitting, activity: initialFormState, loadActivity, clearActivity } = activityStore;
 
-    const [activity, setActivity] = useState<IActivityFormValues>({
-        id: undefined,
-        title: '',
-        category: '',
-        description: '',
-        date: undefined,
-        time: undefined,
-        city: '',
-        venue: ''
-    });
+    const [activity, setActivity] = useState((new ActivityFormValues()))
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        if (match.params.id && activity.id) {
-            loadActivity(match.params.id).then(() => initialFormState && setActivity(initialFormState))
+        if (match.params.id) {
+            setLoading(true)
+            loadActivity(match.params.id).then((activity) => setActivity(new ActivityFormValues(activity))).finally(() => setLoading(false))
         }
-        return () => {
-            clearActivity();
-        }
-    }, [loadActivity, clearActivity, match.params.id, initialFormState, activity.id]);
+    }, [loadActivity, match.params.id]);
 
     // const handleSubmit = () => {
     //     if (activity.id.length === 0) {
@@ -65,9 +55,10 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({ match, hist
             <Grid.Column width={10}>
                 <Segment clearing>
                     <FinalForm
+                    initialValues={activity}
                         onSubmit={handleFinalFormSubmit}
                         render={({handleSubmit}) => (
-                            <Form onSubmit={handleSubmit}>
+                            <Form onSubmit={handleSubmit} loading={loading}>
                             <Field
                                 name='title' 
                                 placeholder="Title"
@@ -118,12 +109,14 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({ match, hist
                             />
                             <Button
                                 loading={submitting}
+                                disabled={loading}
                                 floated='right'
                                 positive type='submit'
                                 content='Submit'
                             />
                             <Button
                                 onClick={() => history.push('/activities')}
+                                disabled={loading}
                                 floated='right'
                                 type='submit'
                                 content='Cancel'
