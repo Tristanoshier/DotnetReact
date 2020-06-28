3fcb7456b78607b6127e4195eda3c0550a1fdb75
Tristanoshier <tsoshier@bsu.edu>
2020-06-28 17:17:00 -0400

    create user object and jwt token generator

--- API/Startup.cs
+++ API/Startup.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Middleware;
 using Application.Activities;
+using Application.Interfaces;
 using Domain;
 using FluentValidation.AspNetCore;
+using Infrastructure.Security;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -56,6 +58,7 @@ namespace API
             var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
             identityBuilder.AddEntityFrameworkStores<DataContext>();
             identityBuilder.AddSignInManager<SignInManager<AppUser>>();
+            services.AddScoped<IJwtGenerator, JwtGenerator>();
 
             services.AddAuthentication();
         }
